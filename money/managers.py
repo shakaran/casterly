85f081b26f37569c94583b4764b75a0963bc9a98
Pablo Recio Quijano <pablo.recioquijano@gmail.com>
2012-06-30 17:29:52 +0200

    Moved the logic to the QuerySet instead of the manager

--- money/managers.py
+++ money/managers.py
@@ -20,22 +20,28 @@ class MovementQuerySet(QuerySet):
     def balance(self):
         return self._amount(self.all())
 
-
-class MovementManager(models.Manager):
-    def get_query_set(self):
-        return MovementQuerySet(self.model)
-
-    def __getattr__(self, name):
-        return getattr(self.get_query_set(), name)
-
     def per_month(self, year, month):
         _, last_day = monthrange(year, month)
-        return self.get_query_set().filter(
-            date__gte=date(year, month, 1),
+        return self.filter(date__gte=date(year, month, 1),
             date__lte=date(year, month, last_day))
 
     def per_week(self, year, week):
         _week = Week(year, week)
         monday = _week.monday()
         sunday = monday + timedelta(6)
-        return self.get_query_set().filter(date__gte=monday, date__lte=sunday)
+        return self.filter(date__gte=monday, date__lte=sunday)
+
+
+class MovementManager(models.Manager):
+
+    def per_month(self, year, month):
+        return self.get_query_set().per_month(year, month)
+
+    def per_week(self, year, week):
+        return self.get_query_set().per_week(year, week)
+
+    def get_query_set(self):
+        return MovementQuerySet(self.model)
+
+    def __getattr__(self, name):
+        return getattr(self.get_query_set(), name)
