2dd9b81aaf19e97f1bcf4601bed74bea6a3b0bad
Pablo Recio Quijano <pablo.recioquijano@gmail.com>
2012-07-08 14:56:26 +0100

    Added more tests for CSV parser

--- money/tests/utils.py
+++ money/tests/utils.py
@@ -14,7 +14,7 @@ LLOYDS_SIMPLE_EXAMPLE_EARN_ROW = """
 06/01/2012,TFR,'00-00-00,0000000,Tickets,,134.3,200"""
 
 
-class CSVParserTest(TestCase):
+class SimpleCSVParserTest(TestCase):
 
 	def setUp(self):
 		super(CSVParserTest, self).setUp()
@@ -50,6 +50,43 @@ class CSVParserTest(TestCase):
 		)
 		self.assertEqual(1, len(data))
 		row = data[0]
+		self.assertEqual(5, len(row.keys()))
+
+		self.assertTrue('date' in row.keys())
+		self.assertTrue('account_number' in row.keys())
+		self.assertTrue('description' in row.keys())
+		self.assertTrue('amount' in row.keys())
+		self.assertTrue('balance' in row.keys())
+
+		self.assertEqual(date(2012, 1, 6), row['date'])
+		self.assertEqual("00-00-00 0000000", row['account_number'])
+		self.assertEqual("Tickets", row['description'])
+		self.assertEqual(134.3, row['amount'])
+		self.assertEqual(200, row['balance'])
+
+	def test_double_rows(self):
+		data = parse_csv(
+			cStringIO.StringIO(
+				LLOYDS_SIMPLE_EXAMPLE_PAY_ROW + LLOYDS_SIMPLE_EXAMPLE_EARN_ROW),
+			parser=LloydsParser,
+		)
+		self.assertEqual(2, len(data))
+		row = data[0]
+		self.assertEqual(5, len(row.keys()))
+
+		self.assertTrue('date' in row.keys())
+		self.assertTrue('account_number' in row.keys())
+		self.assertTrue('description' in row.keys())
+		self.assertTrue('amount' in row.keys())
+		self.assertTrue('balance' in row.keys())
+
+		self.assertEqual(date(2011, 12, 25), row['date'])
+		self.assertEqual("00-00-00 0000000", row['account_number'])
+		self.assertEqual("Christmas presents", row['description'])
+		self.assertEqual(-134.3, row['amount'])
+		self.assertEqual(200, row['balance'])
+
+		row = data[1]
 		self.assertEqual(5, len(row.keys()))
 
 		self.assertTrue('date' in row.keys())
