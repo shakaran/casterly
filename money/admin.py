c6e194d92d07eb14551141677de1f276f1768c46
Pablo Recio <me@pablorecio.com>
2012-08-12 20:18:26 +0100

    Admin for categories

--- money/admin.py
+++ money/admin.py
@@ -1,10 +1,15 @@
 from django.contrib import admin
 
-from money.models import BankAccount
+from money.models import BankAccount, MovementCategory
 
 
 class BankAccountAdmin(admin.ModelAdmin):
     pass
 
 
+class MovementCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(BankAccount, BankAccountAdmin)
+admin.site.register(MovementCategory, MovementCategoryAdmin)
